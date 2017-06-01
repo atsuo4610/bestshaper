7a3a768b305cf9e8ecda682397e91bb19256c100
河内惇夫 <kochiatsuo@kawauchiatsuonoMacBook-Pro.local>
2017-06-01 20:17:08 +0900

    add new file

--- bestshaper2/apps/api.py
+++ bestshaper2/apps/api.py
@@ -5,8 +5,9 @@ import json
 
 @api_view(['POST'])
 def post_wash_num(request):
-    bra = {'wash_num':request.POST['wash_num']}
+
+    bra = request.POST.get['wash_num', 1]
     jsonData = json.loads(bra.decode('utf-8'))
 
-    b = Brassiere(wash_num=jsonData)
+    b = Brassiere(jsonData)
     b.save()
